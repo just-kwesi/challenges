54b5e5b011da1f1a594c6d081cd39e74ef112c2b
Frederick Tetteh <kwesiossom@gmail.com>
2024-02-26 18:56:07 -0600

    feature/video-submission - supabase types added

--- src/app/submit-video/page.tsx
+++ src/app/submit-video/page.tsx
@@ -1,7 +1,24 @@
-export default function SubmitVideo() {
+import { Separator } from '@/components/ui/separator'
+import { createClient } from '@/lib/supabase/server'
+import { redirect } from 'next/navigation'
+import { SubmissionForm } from '@/components/ui/submit-video/video-submission-form'
+
+export default async function SubmitVideo() {
+  const supabase = createClient()
+  const userSession = (await supabase.auth.getSession()).data.session
+  if (!userSession) redirect('/login')
+
   return (
-    <main className="flex justify-center items-center h-screen">
-      <span>Submit Videos</span>
-    </main>
+    <div className="space-y-6 p-10">
+      <div>
+        <h3 className="text-lg font-medium">Video Submission</h3>
+        <p className="text-sm text-muted-foreground">Submit your clip</p>
+      </div>
+      <Separator />
+      <div className="flex flex-col gap-x-7 sm:flex-row">
+        <p>Video Player</p>
+        <SubmissionForm />
+      </div>
+    </div>
   )
 }
---
+++ src/components/ui/submit-video/video-submission-form.tsx
@@ -0,0 +1,26 @@
+'use client'
+import { zodResolver } from '@hookform/resolvers/zod'
+import { useForm } from 'react-hook-form'
+import { z } from 'zod'
+
+import { Button } from '@/components/ui/button'
+import {
+  Form,
+  FormControl,
+  FormDescription,
+  FormField,
+  FormItem,
+  FormLabel,
+  FormMessage,
+} from '@/components/ui/form'
+import { Input } from '@/components/ui/input'
+import { Textarea } from '@/components/ui/textarea'
+import { toast } from '@/components/ui/use-toast'
+
+const videoFormSchema = z.object({})
+
+type VideoFormValues = z.infer<typeof videoFormSchema>
+
+export function SubmissionForm() {
+  return <p>Submission form</p>
+}
